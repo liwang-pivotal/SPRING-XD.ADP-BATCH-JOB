5bad898f91f0f83c472c072308e01ee272e2ada4
Li Wang <liwang@pivotal.io>
2015-08-02 15:02:24 -0400

    change project name

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
 gradle clean build
-cp build/libs/ADP-BATCH-2.jar /Users/lwang/Documents/spring-xd-1.1.2.RELEASE/xd/custom-modules/job/adp.jar
+cp build/libs/ADP-BATCH-JOB.jar /Users/lwang/Documents/spring-xd-1.1.2.RELEASE/xd/custom-modules/job/adp.jar
